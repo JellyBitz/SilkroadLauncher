dbb8ee0ef6191e419d1b8b165c251db61a003f9e
JellyBitz <engelsquintero@gmail.com>
2020-05-11 04:18:42 -0430

    Added patch progress bar Base style done!

--- SilkroadLauncher/ValueConverters/BooleanToVisibilityGoneConverter.cs
+++ SilkroadLauncher/ValueConverters/BooleanToVisibilityGoneConverter.cs
@@ -4,13 +4,13 @@ using System.Windows.Data;
 namespace SilkroadLauncher
 {
     [ValueConversion(typeof(bool), typeof(Visibility))]
-    public class BooleanToInvisibilityConverter : IValueConverter
+    public class BooleanToVisibilityGoneConverter : IValueConverter
     {
-        public static BooleanToInvisibilityConverter Instance = new BooleanToInvisibilityConverter();
+        public static BooleanToVisibilityGoneConverter Instance = new BooleanToVisibilityGoneConverter();
         #region IValueConverter Members
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return (bool)value ? Visibility.Hidden:Visibility.Visible;
+            return (bool)value ? Visibility.Collapsed:Visibility.Visible;
         }
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
