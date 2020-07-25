d0bcdcc40c946b0c705ce4a55bb7b71ad6fa4d95
JellyBitz <engelsquintero@gmail.com>
2020-07-25 16:58:59 -0430

    Added Backrgound border Changed button and loading style

--- SilkroadLauncher/MainWindow.xaml.cs
+++ SilkroadLauncher/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using SilkroadSecurityAPI;
-using System;
-using System.IO;
-using System.IO.Compression;
-using System.Text;
 using System.Windows;
 using System.Windows.Input;
 namespace SilkroadLauncher
