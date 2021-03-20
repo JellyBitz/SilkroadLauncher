710c86fe8e103c4afa33886d639c7c12c6da8073
JellyBitz <engelsquintero@gmail.com>
2021-03-19 20:17:32 -0400

    Fixed Pk2Writer not checking for file existence

--- SilkroadLauncher/Pk2WriterAPI/PK2Writer.cs
+++ SilkroadLauncher/Pk2WriterAPI/PK2Writer.cs
@@ -11,7 +11,16 @@ namespace Pk2WriterAPI
         /// </para>
         /// </summary>
         [DllImport("Pk2Writer.dll", EntryPoint = "_Initialize@4", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
-        public static extern bool Initialize(string gfxDllFilename);
+        public static extern bool _Initialize(string GFXDllFilename);
+        /// <summary>
+        /// Initialize Pk2 writer to execute operations.
+        /// </summary>
+        public static bool Initialize(string GFXDllFilename)
+        {
+            if (System.IO.File.Exists(GFXDllFilename))
+                return _Initialize(GFXDllFilename);
+            return false;
+        }
         /// <summary>
         /// Cleans up GfxFileManager.DLL. This function must be called before the
         /// program exits and after Close if a PK2 file was opened.
@@ -35,13 +44,15 @@ namespace Pk2WriterAPI
         /// </para>
         /// </summary>
         [DllImport("Pk2Writer.dll", EntryPoint = "_Open@12", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
-        private static extern bool Open(string Pk2Filename, string AccessKey, int AccessKeyLen);
+        private static extern bool _Open(string pk2Filename, string AccessKey, int accessKeyLen);
         /// <summary>
-        /// Opens a PK2 file for writing.
+        /// Opens a Pk2 file for writing.
         /// </summary>
         public static bool Open(string Pk2Filename, string BlowfishKey)
         {
-            return Open(Pk2Filename, BlowfishKey, BlowfishKey.Length);
+            if (System.IO.File.Exists(Pk2Filename))
+                return _Open(Pk2Filename, BlowfishKey, BlowfishKey.Length);
+            return false;
         }
         /// <summary>
         /// Closes an opened PK2 file. This function must be called before the program exits and before Deinitialize is called.
@@ -59,6 +70,15 @@ namespace Pk2WriterAPI
         /// </summary>
         /// <param name="entryFilename">The full path to the file into the PK2</param>
         [DllImport("Pk2Writer.dll", EntryPoint = "_ImportFile@8", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
-        public static extern bool ImportFile(string entryFilename, string inputFilename);
+        public static extern bool _ImportFile(string entryFilename, string inputFilename);
+        /// <summary>
+        /// Imports a file into the Pk2.
+        /// </summary>
+        public static bool ImportFile(string EntryFilename, string InputFilename)
+        {
+            if (System.IO.File.Exists(InputFilename))
+                return _ImportFile(EntryFilename, InputFilename);
+            return false;
+        }
     }
 }
