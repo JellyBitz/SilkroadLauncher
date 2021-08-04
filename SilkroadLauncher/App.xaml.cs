69a906bb9bc61b96835d7a17442e070e09485825
Engels Quintero <engelsquintero@gmail.com>
2021-08-04 03:07:36 -0400

    Added original launcher behavior when another one is running

--- SilkroadLauncher/App.xaml.cs
+++ SilkroadLauncher/App.xaml.cs
@@ -1,17 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Threading;
 using System.Windows;
 
 namespace SilkroadLauncher
 {
     /// <summary>
-    /// Lógica de interacción para App.xaml
+    /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
-    }
+        #region Public Properties
+        /// <summary>
+        /// Keeps instance creation from this application
+        /// </summary>
+        public static Mutex Mutex { get; private set; } = null;
+		#endregion
+
+		#region Private Helpers
+		protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Check the instance lock
+            var thisProcess = Process.GetCurrentProcess();
+            Mutex = new Mutex(true, thisProcess.MainModule.FileVersionInfo.ProductName, out bool IsFirstTime);
+
+            if (!IsFirstTime)
+            {
+                // Find all process with the same description
+                var processes = Process.GetProcesses();
+                foreach (var p in processes)
+                {
+                    try
+                    {
+                        // Check the process and close it
+                        if (thisProcess.MainModule.FileVersionInfo.ProductName == p.MainModule.FileVersionInfo.ProductName && thisProcess.Id != p.Id)
+                            p.Kill();
+                    }
+                    catch
+                    {
+                        // Ignore this process, it's not from the same platform.
+                    }
+                }
+            }
+        }
+		#endregion
+	}
 }
